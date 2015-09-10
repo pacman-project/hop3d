01f6e423a470fb9e5bf02ad14a7e996882935d7e
christow <christow@poczta.onet.pl>
2015-09-10 18:14:42 +0100

    Reading xml files for filters half backed.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
-#include "include/ImageFilter/depthImageFilter.h"
+
+#include "Data/Defs.h"
+#include "Utilities/Reader.h"
+#include "ImageFilter/depthImageFilter.h"
+
 
 int main(void){
-    ImageFilter *imageFilter;
-    imageFilter = createDepthImageFilter("depthFilter.xml");
+    hop3d::ImageFilter *imageFilter;
+    imageFilter = hop3d::createDepthImageFilter("depthFilter.xml");
     std::cout << imageFilter->getName() << "\n";
     std::cout << "Hello World" << std::endl;
+    std::vector<hop3d::Filter> filters;
+    hop3d::Reader reader;
+    reader.readFilters("filters_7x7_0_005.xml","normals_7x7_0_005.xml",filters);
     return 0;
 }
 
