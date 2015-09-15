52f30021623951265068af67f2d982e3a2caaa1b
christow <christow@poczta.onet.pl>
2015-09-15 17:31:16 +0100

    Filters normalized and runing through the response image with octets done.

--- src/ImageFilter/depthImageFilter.cpp
+++ src/ImageFilter/depthImageFilter.cpp
@@ -40,6 +40,7 @@ void DepthImageFilter::computeOctets(const cv::Mat& depthImage, hop3d::Octet::Se
     octets.size();//!!
 
     int filterSize = filters[0].patch.cols;
+    int overlapRf = 1; //overlap of the receptive fields in octets
     int offset = int(filterSize/2); //casting discards fractional part
     if (depthImage.depth() == CV_16U) std::cout << "CV_16U == unsigned short " << sizeof(unsigned short) << std::endl;
 
@@ -50,7 +51,7 @@ void DepthImageFilter::computeOctets(const cv::Mat& depthImage, hop3d::Octet::Se
     hop3d::ImagesDisplay displayer;
     displayer.displayDepthImage(depthImageDouble);
     std::vector<cv::Mat> filterImages;
-
+    //applying all the filters to an image
     for(int iterF = 0 ; iterF < filters.size(); iterF++)
     {
         cv::Mat loadedFilter;
@@ -62,12 +63,12 @@ void DepthImageFilter::computeOctets(const cv::Mat& depthImage, hop3d::Octet::Se
         int numActive = cv::countNonZero(loadedMask);
 //        std::cout << "Filter " << ": "<< std::endl << loadedFilter << std::endl << std::endl;
 //        std::cout << "Mask active "<< numActive  << " : "<< std::endl << loadedMask << std::endl << std::endl;
-        cv::Mat filterImage(depthImage.rows-(2*offset),depthImage.cols-(2*offset), cv::DataType<double>::type,cv::Scalar(0));
+        //boundary of the image is not taken into account
+        cv::Mat filterImage(depthImage.rows-(offset),depthImage.cols-(offset), cv::DataType<double>::type,cv::Scalar(0));
         std::vector<double> sValues;
-        for(int i = offset; i < depthImageDouble.rows-(2*offset);i++){
-            for(int j = offset; j < depthImageDouble.cols-(2*offset);j++){
-//        for(int i = 175; i < 195;i++){
-//            for(int j = 175; j < 195;j++){
+        //looping over whole image
+        for(int i = offset; i < depthImageDouble.rows-(offset);i++){
+            for(int j = offset; j < depthImageDouble.cols-(offset);j++){
                 cv::Mat responseTemp(filterSize,filterSize, cv::DataType<double>::type);
                 cv::Mat imageRoi(filterSize,filterSize, cv::DataType<double>::type);
                 cv::Mat subResult(filterSize,filterSize, cv::DataType<double>::type);
@@ -87,8 +88,11 @@ void DepthImageFilter::computeOctets(const cv::Mat& depthImage, hop3d::Octet::Se
                 responseTemp = cv::abs(responseTemp);
 //                std::cout << "responseTemp " << i << "; " << j << ": " << std::endl << responseTemp << std::endl << std::endl;
                 double s = cv::sum(responseTemp)[0];
+                //condition not to take into account boundaries of the object
                 if (s > 0.8 || s < -0.1) s = 0;
+                //relation to number of active elements in the filter + scaling to get rid of problems with small floating point values
                 else s *=(10000/numActive);
+
                 //std::cout << "Value of s: " << i << "; " << j << ": " << s << std::endl;
 
                 filterImage.at<double>(i-offset,j-offset) = s;
@@ -111,21 +115,72 @@ void DepthImageFilter::computeOctets(const cv::Mat& depthImage, hop3d::Octet::Se
 
 //        std::cout << "Image:" << std::endl << filterImages[iterI] << std::endl << std::endl;
         //displayer.displayDepthImage(filterImages[iterI]);
-        cv::Mat responseImage(depthImage.rows-(2*offset),depthImage.cols-(2*offset), cv::DataType<double>::type,cv::Scalar(0));
-        cv::Mat idImage(depthImage.rows-(2*offset),depthImage.cols-(2*offset), cv::DataType<int>::type,cv::Scalar(0));
+        cv::Mat responseImage(depthImage.rows-(offset),depthImage.cols-(offset), cv::DataType<double>::type,cv::Scalar(0));
+        cv::Mat idImage(depthImage.rows-(offset),depthImage.cols-(offset), cv::DataType<int>::type,cv::Scalar(0));
+        std::vector<double> maxResponses;
         for(int i = 0; i < responseImage.rows;i++){
             for(int j = 0; j < responseImage.cols;j++){
                 std::vector<double> minResponses;
                 for(int iterI = 0 ; iterI < filterImages.size(); iterI++){
                         minResponses.push_back(filterImages[iterI].at<double>(j,i));
                 }
-                auto it = std::min_element(minResponses.begin(),minResponses.end());
-                responseImage.at<double>(j,i) = *it;
-                idImage.at<int>(j,i) = it - minResponses.begin();
+                auto itMin = std::min_element(minResponses.begin(),minResponses.end());
+                auto itMax = std::max_element(minResponses.begin(),minResponses.end());
+                maxResponses.push_back(*itMax);
+                responseImage.at<double>(j,i) = *itMin;
+                idImage.at<int>(j,i) = itMin - minResponses.begin();
             }
+
          }
+         auto max = std::max_element(maxResponses.begin(),maxResponses.end());
+         double maxValue = *max;
+         responseImage = responseImage.mul(cv::Scalar(-1));
+         responseImage = responseImage + maxValue;
+         responseImage = responseImage.mul(cv::Scalar(1/maxValue));
          displayer.displayDepthImage(responseImage);
          displayer.displayDepthImage(idImage);
+
+
+         int octetSize = (filterSize-overlapRf)*3;
+         int octetOffset = int(octetSize/2);
+         for(int i = octetOffset; i < responseImage.rows-(octetOffset-2*overlapRf);i+=(octetSize-overlapRf)){
+            for(int j = octetOffset; j < responseImage.cols-(octetOffset-2*overlapRf);j+=(octetSize-overlapRf)){
+                cv::Mat imageRoi(octetSize,octetSize, cv::DataType<double>::type);
+                cv::Mat idRoi(octetSize,octetSize, cv::DataType<int>::type);
+                cv::Rect regionOfInterest = cv::Rect(j-octetOffset, i-octetOffset,octetSize,octetSize);
+                //have to use clone otherwise it will go along different blocks of memory after performing substract
+                //simple ROI is just a change of header to the same file when = is used it will start to point to the data in this other Mat
+                imageRoi = responseImage(regionOfInterest).clone();
+                idRoi = idImage(regionOfInterest).clone();
+                hop3d::Octet octetTemp;
+                int octetIter = 0;
+//                for(int k = offset; k < imageRoi.rows-(offset);k+=(offset-overlapRf)){
+//                    for(int l = offset; l < imageRoi.cols-(offset);l+=(offset-overlapRf)){
+//                        cv::Mat octetRoi(filterSize,filterSize, cv::DataType<double>::type);
+//                        cv::Mat octetIdRoi(filterSize,filterSize, cv::DataType<int>::type);
+//                        cv::Rect subRegion = cv::Rect(l-offset, k-offset,filterSize,filterSize);
+//                        octetRoi = imageRoi(subRegion).clone();
+//                        octetIdRoi = idRoi(subRegion).clone();
+//                        double minVal;
+//                        double maxVal;
+//                        cv::Point minLoc;
+//                        cv::Point maxLoc;
+//                        //location of the maximum point in the subelement of the octet
+//                        cv::minMaxLoc( octetRoi, &minVal, &maxVal, &minLoc, &maxLoc );
+//                        maxLoc.x +=(l+j+octetOffset+offset);
+//                        maxLoc.y +=(k+i+octetOffset+offset);
+//                        double depthPoint;
+//                        depthPoint = depthImageDouble.at<double>(maxLoc);
+//                        octetTemp.filterPos[octetIter/3][octetIter%3] = hop3d::ImageCoordsDepth(maxLoc.x,maxLoc.y,depthPoint);
+//                        octetRoi.release();
+//                    }
+//                }
+
+              imageRoi.release();
+              idRoi.release();
+             }
+         }
+
 
 
 }
@@ -149,6 +204,11 @@ void DepthImageFilter::setFilters(std::string patchesFileName, std::string norma
 {
     hop3d::Reader reader;
     reader.readFilters(patchesFileName,normalsFileName,masksFileName,filters);
+}
+
+int DepthImageFilter::filterSingleImageSingleFilter(const cv::Mat &depthImage, Filter &filter, cv::Mat &filteredImage)
+{
+
 }
 
 hop3d::ImageFilter* hop3d::createDepthImageFilter(void) {
