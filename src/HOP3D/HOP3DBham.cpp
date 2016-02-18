574721e4aedc02058baf756ab0d67b7b65c79bfc
dominikbelter <dominik.belter@gmail.com>
2016-02-18 18:45:00 +0100

    debug

--- src/HOP3D/HOP3DBham.cpp
+++ src/HOP3D/HOP3DBham.cpp
@@ -351,7 +351,9 @@ void HOP3DBham::learn(){
             for (size_t categoryNo=0;categoryNo<datasetInfo.categories.size();categoryNo++){
                 for (size_t objectNo=0;objectNo<datasetInfo.categories[categoryNo].objects.size();objectNo++){
                     for (size_t imageNo=0;imageNo<datasetInfo.categories[categoryNo].objects[objectNo].images.size();imageNo++){
+                        std::cout << layerNo << " " << overlapNo << " " << categoryNo << " " << objectNo << " " << imageNo <<"\n";
                         imageFilterer->computePartsImage(overlapNo, (int)categoryNo, (int)objectNo, (int)imageNo, *hierarchy, (int)layerNo);
+                        std::cout << "end\n";
                     }
                 }
             }
