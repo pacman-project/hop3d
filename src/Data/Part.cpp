21ef7c177e5a59ffe670f3df672a0c20f894c4db
dominikbelter <dominik.belter@gmail.com>
2016-05-26 19:48:36 +0200

    load new format

--- src/Data/Part.cpp
+++ src/Data/Part.cpp
@@ -1509,13 +1509,13 @@ std::istream& operator>>(std::istream& is, ViewIndependentPart& part){
             }
         }
     }
-//    for (int i=0;i<3;i++){
-//        for (int j=0;j<3;j++){
-//            for (int k=0;k<3;k++){
-//                is >> part.offsets[i][j][k];
-//            }
-//        }
-//    }
+    for (int i=0;i<3;i++){
+        for (int j=0;j<3;j++){
+            for (int k=0;k<3;k++){
+                is >> part.offsets[i][j][k];
+            }
+        }
+    }
     for (int i=0;i<3;i++){
         for (int j=0;j<3;j++){
             for (int k=0;k<3;k++){
